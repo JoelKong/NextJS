f0cf240ba09ed271de7dd3f744ad4f093932393b
Joel Kong <joelkong2001@gmail.com>
2023-01-03 12:21:30 +0800

    feat: finished main featured page

---
+++ eventsproject/components/events/EventList.js
@@ -0,0 +1,22 @@
+import EventItem from "./EventItem";
+
+function EventList({ items }) {
+  return (
+    <ul>
+      {items.map((item) => {
+        return (
+          <EventItem
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            location={item.location}
+            date={item.date}
+            image={item.image}
+          />
+        );
+      })}
+    </ul>
+  );
+}
+
+export default EventList;
