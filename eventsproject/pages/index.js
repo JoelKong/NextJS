1cc18fa22c68644496ad7f5c7170f4fe3a9be8c1
Joel Kong <joelkong2001@gmail.com>
2023-01-05 20:24:21 +0800

    chore: learnt about adding head element

--- eventsproject/pages/index.js
+++ eventsproject/pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { getFeaturedEvents } from "../dummy-data";
 import EventList from "../components/events/EventList";
 
@@ -5,6 +6,13 @@ function HomePage() {
   const featuredEvents = getFeaturedEvents();
   return (
     <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name="description"
+          content="Find a lot of great events that allow you to evolve"
+        />
+      </Head>
       <EventList items={featuredEvents} />
     </div>
   );
