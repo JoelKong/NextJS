796baee8903885cccfbc6d53ac5475e269552dc9
Joel Kong <joelkong2001@gmail.com>
2023-01-05 21:28:54 +0800

    chore: learnt about the api folder

--- api/pages/index.js
+++ api/pages/index.js
@@ -1,7 +1,29 @@
+import { useRef } from "react";
+
 function HomePage() {
+  const emailInputRef = useRef();
+  const FeedbackInputRef = useRef();
+
+  function submitFormHandler(e) {
+    e.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = FeedbackInputRef.current.value;
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea id="feedback" rows="5" ref={FeedbackInputRef}></textarea>
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
   );
 }
