c4cbc9f19440ba3a8c027d2fcb6f36d0691101a8
Joel Kong <joelkong2001@gmail.com>
2023-01-05 20:29:38 +0800

    chore: added head for events page

--- eventsproject/pages/events/index.js
+++ eventsproject/pages/events/index.js
@@ -1,5 +1,19 @@
+import Head from "next/head";
+import { Fragment } from "react";
+
 function AllEventsPage() {
-  return <div>sdf</div>;
+  return (
+    <Fragment>
+      <Head>
+        <title>All Events</title>
+        <meta
+          name="description"
+          content="Find a lot of great events that allow you to evolve"
+        />
+      </Head>
+      <p>sdf</p>;
+    </Fragment>
+  );
 }
 
 export default AllEventsPage;
