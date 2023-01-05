8546641809b19ec8ce6e3cb26f1cc3894ac9251a
Joel Kong <joelkong2001@gmail.com>
2023-01-05 20:39:38 +0800

    chore: setup head in _app.js

--- eventsproject/pages/_app.js
+++ eventsproject/pages/_app.js
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import "../styles/globals.css";
 import Layout from "../components/layout/Layout";
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Component {...pageProps} />;
     </Layout>
   );
