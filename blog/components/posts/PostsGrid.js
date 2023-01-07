66017e699f657c67a1cf12b02f15e011ae8f1586
Joel Kong <joelkong2001@gmail.com>
2023-01-07 15:11:32 +0800

    feat: setup postgrid

--- blog/components/posts/PostsGrid.js
+++ blog/components/posts/PostsGrid.js
@@ -1,3 +1,16 @@
-function PostsGrid() {}
+import PostItem from "./PostItem";
+import classes from "./postsgrid.module.css";
+
+function PostsGrid(props) {
+  const { posts } = props;
+
+  return (
+    <ul className={classes.grid}>
+      {posts.map((post) => (
+        <PostItem />
+      ))}
+    </ul>
+  );
+}
 
 export default PostsGrid;
