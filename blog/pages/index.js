e9b02a3c4264bbdf25add93dd4fd292e16ce7b92
Joel Kong <joelkong2001@gmail.com>
2023-01-07 15:33:02 +0800

    feat: finished setting up featured posts with dummy data

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -1,11 +1,47 @@
 import { Fragment } from "react";
 import Hero from "../components/homepage/Hero";
+import FeaturedPosts from "../components/homepage/Featuredposts";
+
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-with-nextjs",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is the React Framework for production which introduces file based routing and helps with server side and pre rendering.",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started-with-nextjs2",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is the React Framework for production which introduces file based routing and helps with server side and pre rendering.",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started-with-nextjs3",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is the React Framework for production which introduces file based routing and helps with server side and pre rendering.",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started-with-nextjs4",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is the React Framework for production which introduces file based routing and helps with server side and pre rendering.",
+    date: "2022-02-10",
+  },
+];
 
 function HomePage() {
   return (
     <Fragment>
       <Hero />
-      {/* <FeaturedPosts /> */}
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </Fragment>
   );
 }
